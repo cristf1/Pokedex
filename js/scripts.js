e5e6e43e32c1cbf65dd105281465269b3d5b211f
cristf1 <cristf1@users.noreply.github.com>
2022-07-26 15:43:37 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,12 @@ pokemonList= [
   {name:'Bulbasaur', height: 7, types:'grass','poison'},
   {name:'Charmander', height: 6, types:'fire'},
   {name:'Squirtle', height: 5, types:'water'}
-]
+];
+
+alert('Hi!')
+
+//document.write(pokemonList[1])
+
+//for (let i=0; i <= pokemonList.length; i++){
+  //document.write(pokemonList.name[i]+ " /(height: " + pokemonList.height[i] + ")" );
+//}
