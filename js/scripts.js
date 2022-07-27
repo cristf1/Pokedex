7f967c772426e7c307e25e9426279d36a8d713bc
cristf1 <cristf1@users.noreply.github.com>
2022-07-26 17:20:41 -0700

    Update scripts.js to list pokedex

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
 pokemonList= [
-  {name:'Bulbasaur', height: 7, types:'grass','poison'},
+  {name:'Bulbasaur', height: 7, types:['grass','poison']},
   {name:'Charmander', height: 6, types:'fire'},
   {name:'Squirtle', height: 5, types:'water'}
 ];
 
-alert('Hi!')
 
-//document.write(pokemonList[1])
 
-//for (let i=0; i <= pokemonList.length; i++){
-  //document.write(pokemonList.name[i]+ " /(height: " + pokemonList.height[i] + ")" );
-//}
+for (let i=0; i <= pokemonList.length; i++){
+  document.write("<br>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ")" );
+  if (pokemonList[i].height > 6){
+    document.write(" - Wow that\'s big!")
+  }
+};
