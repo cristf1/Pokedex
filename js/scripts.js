0cfc42e27b8d2af4ac52263ab017c943c5a3d3fe
cristf1 <cristf1@users.noreply.github.com>
2022-07-28 23:25:08 -0700

    Updated with forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,16 @@
-pokemonList= [
+let pokemonList= [
   {name:'Bulbasaur', height: 7, types:['grass','poison']},
   {name:'Charmander', height: 6, types:'fire'},
   {name:'Squirtle', height: 5, types:'water'}
 ];
 
+pokemonList.forEach(pokemonPrint)
 
-
-for (let i=0; i <= pokemonList.length; i++){
-  document.write("<br>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ")" );
-  if (pokemonList[i].height > 6){
-    document.write(" - Wow that\'s big!")
+function pokemonPrint(pokemon){
+  document.write("<br>" + pokemon.name + " (height: " + pokemon.height + ")" );
+    if (pokemon.height > 6){
+      document.write(" - Wow that\'s big!")
+      }
+    else {"<br>"
   }
 };
