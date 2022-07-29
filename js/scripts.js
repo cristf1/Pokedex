8f7a1bbb9dd2334dcd775a5b7e339d34c96285a2
cristf1 <cristf1@users.noreply.github.com>
2022-07-29 11:44:54 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,36 @@
-let pokemonList= [
-  {name:'Bulbasaur', height: 7, types:['grass','poison']},
-  {name:'Charmander', height: 6, types:'fire'},
-  {name:'Squirtle', height: 5, types:'water'}
-];
+let pokemonRepository = (function (){
 
-pokemonList.forEach(pokemonPrint)
 
-function pokemonPrint(pokemon){
-  document.write("<br>" + pokemon.name + " (height: " + pokemon.height + ")" );
-    if (pokemon.height > 6){
-      document.write(" - Wow that\'s big!")
-      }
-    else {"<br>"
+  let pokemonList= [
+    {name:'Bulbasaur', height: 7, types:['grass','poison']},
+    {name:'Charmander', height: 6, types:'fire'},
+    {name:'Squirtle', height: 5, types:'water'}
+  ];
+
+  pokemonList.forEach(pokemonPrint)
+
+  function pokemonPrint(pokemon){
+    document.write("<br>" + pokemon.name + " (height: " + pokemon.height + ")" );
+      if (pokemon.height > 6){
+        document.write(" - Wow that\'s big!")
+        }
+      else {"<br>"
+    }
+  };
+
+  function getAll(){
+    return pokemonList;
   }
-};
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  return {
+    getAll: getAll,
+    add: add,
+  }
+
+})();
+
+pokemonRepository.getAll();
