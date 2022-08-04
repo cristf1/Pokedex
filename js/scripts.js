bf839e0b3cd15a50054fbac2d368efd6a6541d7a
cristf1 <cristf1@users.noreply.github.com>
2022-08-04 14:43:41 -0700

    Update for Task 1.6

--- js/scripts.js
+++ js/scripts.js
@@ -7,16 +7,20 @@ let pokemonRepository = (function (){
     {name:'Squirtle', height: 5, types:'water'}
   ];
 
-  pokemonList.forEach(pokemonPrint)
+  function addListItem(pokemon){
+    let pokemonOrder= document.querySelector('.pokemon-list');
+    let listItem= document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText= pokemon.name;
+    button.classList.add('button-class');
+    listItem.appendChild(button);
+    pokemonOrder.appendChild(listItem);
+    button.addEventListener('click', function() {showDetails(pokemon)});
+  }
 
-  function pokemonPrint(pokemon){
-    document.write("<br>" + pokemon.name + " (height: " + pokemon.height + ")" );
-      if (pokemon.height > 6){
-        document.write(" - Wow that\'s big!")
-        }
-      else {"<br>"
-    }
-  };
+  function showDetails(pokemon){
+    console.log(pokemon);
+  }
 
   function getAll(){
     return pokemonList;
@@ -29,8 +33,13 @@ let pokemonRepository = (function (){
   return {
     getAll: getAll,
     add: add,
+    addListItem: addListItem,
   }
 
 })();
 
-pokemonRepository.getAll();
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function(pokemon){
+  pokemonRepository.addListItem(pokemon);
+});
